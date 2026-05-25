8b6ec1330d5a798e4783735fe9e9f9bd34d40094
--global <calebwilliams@ssw.com.au>
2026-05-26 08:04:07 +1000

    Made sizing consistent for install window

--- scripts/form-component.tsx
+++ scripts/form-component.tsx
@@ -127,7 +127,7 @@ export function FormContent({ targetRepo, token }: FormProps) {
   return (
     <>
     <div className="absolute inset-0 bg-cover -z-1 bg-[url('/polygon-bg.svg')] mix-blend-color-burn opacity-35" aria-hidden="true" />
-    <Card className="relative max-w-[540px] mx-auto my-16">
+    <Card className="relative max-w-xl mx-auto my-16">
       <CardContent>
         {loadingSecrets && (
           <div
