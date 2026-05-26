47c543c099d64e0195892cf2571b9e0d8d79032f
--global <calebwilliams@ssw.com.au>
2026-05-26 11:39:43 +1000

    importing react properly

--- scripts/components/spinner.tsx
+++ scripts/components/spinner.tsx
@@ -1,6 +1,6 @@
 import { LoaderCircle } from "lucide-react";
 import { cn } from "../lib/utils";
-import * as React from "react";
+import React from "react";
 
 export function Spinner({ size = 15, className }: { size?: number; className?: string }) {
   return <LoaderCircle size={size} className={cn("animate-spin shrink-0", className)} />;
